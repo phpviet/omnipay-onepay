313b039ab8c13fd86ef759ac1ce400ff7be793bd
Vuong Minh <vuongxuongminh@gmail.com>
2019-07-03 00:10:38 +0700

    Added gateway apis

--- src/InternationalGateway.php
+++ src/InternationalGateway.php
@@ -8,6 +8,10 @@
 
 namespace Omnipay\OnePay;
 
+use Omnipay\OnePay\Message\IncomingRequest;
+use Omnipay\OnePay\Message\International\PurchaseRequest;
+use Omnipay\OnePay\Message\International\QueryTransactionRequest;
+
 /**
  * @author Vuong Minh <vuongxuongminh@gmail.com>
  * @since 1.0.0
@@ -20,5 +24,41 @@ class InternationalGateway extends AbstractGateway
     public function getName(): string
     {
         return 'OnePay International';
+    }
+
+    /**
+     *{@inheritdoc}
+     * @return \Omnipay\Common\Message\AbstractRequest|PurchaseRequest
+     */
+    public function purchase(array $options = []): PurchaseRequest
+    {
+        return $this->createRequest(PurchaseRequest::class, $options);
+    }
+
+    /**
+     *{@inheritdoc}
+     * @return \Omnipay\Common\Message\AbstractRequest|IncomingRequest
+     */
+    public function completePurchase(array $options = []): IncomingRequest
+    {
+        return $this->createRequest(IncomingRequest::class, $options);
+    }
+
+    /**
+     *{@inheritdoc}
+     * @return \Omnipay\Common\Message\AbstractRequest|IncomingRequest
+     */
+    public function notification(array $options = []): IncomingRequest
+    {
+        return $this->createRequest(IncomingRequest::class, $options);
+    }
+
+    /**
+     *{@inheritdoc}
+     * @return \Omnipay\Common\Message\AbstractRequest|QueryTransactionRequest
+     */
+    public function queryTransaction(array $options = []): QueryTransactionRequest
+    {
+        return $this->createRequest(QueryTransactionRequest::class, $options);
     }
 }
