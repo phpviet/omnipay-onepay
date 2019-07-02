dfab6dbe41cc446790f28375535e9f6716b3e4d1
Vuong Minh <vuongxuongminh@gmail.com>
2019-07-02 22:35:57 +0700

    Added international purchase request

---
+++ src/Message/International/PurchaseRequest.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * @link https://github.com/phpviet/omnipay-onepay
+ *
+ * @copyright (c) PHP Viet
+ * @license [MIT](https://opensource.org/licenses/MIT)
+ */
+
+namespace Omnipay\OnePay\Message\International;
+
+use Omnipay\OnePay\Message\AbstractPurchaseRequest;
+
+/**
+ * @author Vuong Minh <vuongxuongminh@gmail.com>
+ * @since 1.0.0
+ */
+class PurchaseRequest extends AbstractPurchaseRequest
+{
+    protected $testEndpoint = 'https://onepay.vn/vpcpay/vpcpay.op';
+
+    protected $productionEndpoint = 'https://mtf.onepay.vn/vpcpay/vpcpay.op';
+}
